3d6e77099a7d4978dc70fa8fb44cfec03793328f
delsehi <prskati@gmail.com>
2023-06-22 17:09:14 +0200

    Support basic CREATE PROCEDURE of MSSQL (#900)
    Co-authored-by: Andrew Lamb <andrew@nerdnetworks.org>

--- src/keywords.rs
+++ src/keywords.rs
@@ -702,6 +702,7 @@ pub const RESERVED_FOR_TABLE_ALIAS: &[Keyword] = &[
     Keyword::SET,
     Keyword::QUALIFY,
     Keyword::WINDOW,
+    Keyword::END,
 ];
 
 /// Can't be used as a column alias, so that `SELECT <expr> alias`
@@ -731,4 +732,5 @@ pub const RESERVED_FOR_COLUMN_ALIAS: &[Keyword] = &[
     // Reserved only as a column alias in the `SELECT` clause
     Keyword::FROM,
     Keyword::INTO,
+    Keyword::END,
 ];
