5d0fbcdff757aed9d66634aef17c4333dadbe63f
Ophir LOJKINE <pere.jobs@gmail.com>
2023-07-27 14:32:55 +0300

    Fix "BEGIN TRANSACTION" being serialized as "START TRANSACTION" (#935)

--- src/parser.rs
+++ src/parser.rs
@@ -6952,6 +6952,7 @@ impl<'a> Parser<'a> {
         self.expect_keyword(Keyword::TRANSACTION)?;
         Ok(Statement::StartTransaction {
             modes: self.parse_transaction_modes()?,
+            begin: false,
         })
     }
 
@@ -6959,6 +6960,7 @@ impl<'a> Parser<'a> {
         let _ = self.parse_one_of_keywords(&[Keyword::TRANSACTION, Keyword::WORK]);
         Ok(Statement::StartTransaction {
             modes: self.parse_transaction_modes()?,
+            begin: true,
         })
     }
 
