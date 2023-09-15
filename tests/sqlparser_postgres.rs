f23ef8b142e5a891934d16eaf1d1459ba53e8b28
Amogh-Bharadwaj <amogh@peerdb.io>
2023-09-15 15:57:41 +0530

    lint v2

--- tests/sqlparser_postgres.rs
+++ tests/sqlparser_postgres.rs
@@ -2992,7 +2992,7 @@ fn parse_create_single_mirror_no_options() {
 fn parse_create_single_mirror() {
     match pg().verified_stmt("CREATE MIRROR IF NOT EXISTS test_mirror FROM p1 TO p2 WITH TABLE MAPPING (s1.t1:s2.t2) WITH (key1 = 'value1')") {
          Statement::CreateMirror { if_not_exists,create_mirror: CDC(cdc) } => {
-            assert_eq!(if_not_exists, true);
+            assert!(if_not_exists);
             assert_eq!(cdc.mirror_name, ObjectName(vec![Ident::new("test_mirror")]));
             assert_eq!(cdc.source_peer, ObjectName(vec![Ident::new("p1")]));
             assert_eq!(cdc.target_peer, ObjectName(vec![Ident::new("p2")]));
@@ -3011,7 +3011,7 @@ fn parse_create_single_mirror() {
 fn parse_create_multi_mirror() {
     match pg().verified_stmt("CREATE MIRROR test_mirror FROM p1 TO p2 WITH TABLE MAPPING (s1.t1:s2.t2, s1.t3:s2.t4) WITH (key1 = 'value1', key2 = 'value2')") {
          Statement::CreateMirror { if_not_exists,create_mirror: CDC(cdc) } => {
-            assert_eq!(if_not_exists, false);
+            assert!(!if_not_exists);
             assert_eq!(cdc.mirror_name, ObjectName(vec![Ident::new("test_mirror")]));
             assert_eq!(cdc.source_peer, ObjectName(vec![Ident::new("p1")]));
             assert_eq!(cdc.target_peer, ObjectName(vec![Ident::new("p2")]));
@@ -3034,7 +3034,7 @@ fn parse_create_multi_mirror() {
 fn parse_mirror_for_select() {
     match pg().verified_stmt("CREATE MIRROR IF NOT EXISTS test_mirror FROM p1 TO p2 FOR $$SELECT 1$$ WITH (key1 = 'value1', key2 = 'value2')") {
          Statement::CreateMirror { if_not_exists,create_mirror: MirrorSelect(select) } => {
-            assert_eq!(if_not_exists, true);
+            assert!(if_not_exists);
             assert_eq!(select.mirror_name, ObjectName(vec![Ident::new("test_mirror")]));
             assert_eq!(select.source_peer, ObjectName(vec![Ident::new("p1")]));
             assert_eq!(select.target_peer, ObjectName(vec![Ident::new("p2")]));
